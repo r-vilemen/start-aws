48a8d086393ad40caa920562f48904a0f39820ab
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2026-03-02 22:45:10 -0300

    add question 1

---
+++ Atividades/q1.py
@@ -0,0 +1,8 @@
+# Par ou Ímpar: Leia um número inteiro e informe se ele é par ou ímpar.
+
+num = int(input("Digite um número:"))
+
+if num % 2 == 0:
+    print("O {num} é par!")
+else:
+    print("o {num} é ímpar!")
